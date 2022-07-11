2d117c2c4d65ddc93a2791ec33b5ef72bd00a6b9
xu5343 <32597757+xu5343@users.noreply.github.com>
2022-07-11 22:50:41 +0800

    Update preset.sh

--- preset.sh
+++ preset.sh
@@ -20,7 +20,7 @@ RED='\033[01;31m'
 RESET='\033[0m'
 INSTALL_SDIR='/usr/src/ffmpegscript'
 _url=`cat ./url.txt`
-SOURCE_URL=$_url/presets
+SOURCE_URL=$_url
 INSTALL_DDIR='/usr/local/cpffmpeg'
 export cpu=`cat "/proc/cpuinfo" | grep "processor"|wc -l`
 export TMPDIR=$HOME/tmp
@@ -31,7 +31,6 @@ echo -e $RED"Installation of $_package ....... started"$RESET
 cd $INSTALL_SDIR/
 rm -rf presets*
 wget $SOURCE_URL/$_package
-#mv /root/ffmpeginstaller/source/presets/$_package $INSTALL_SDIR
 tar -xvzf $_package
 cp -vrf presets/* /usr/local/cpffmpeg/share/ffmpeg/
 cp -vrf presets/* /usr/share/ffmpeg/
