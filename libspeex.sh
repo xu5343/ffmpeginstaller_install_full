ec2fe06c5c362db8d86f2140d62b0313c09f161c
xu5343 <32597757+xu5343@users.noreply.github.com>
2022-07-11 22:57:22 +0800

    Update libspeex.sh

--- libspeex.sh
+++ libspeex.sh
@@ -20,7 +20,7 @@ RED='\033[01;31m'
 RESET='\033[0m'
 INSTALL_SDIR='/usr/src/ffmpegscript'
 _url=`cat ./url.txt`
-SOURCE_URL=$_url/libspeex
+SOURCE_URL=$_url
 INSTALL_DDIR='/usr/local/cpffmpeg'
 export cpu=`cat "/proc/cpuinfo" | grep "processor"|wc -l`
 export TMPDIR=$HOME/tmp
@@ -31,7 +31,6 @@ echo -e $RED"Installation of $_package ....... started"$RESET
 cd $INSTALL_SDIR/
 rm -rf speex*
 wget $SOURCE_URL/$_package
-#mv /root/ffmpeginstaller/source/libspeex/$_package $INSTALL_SDIR
 tar -xvzf $_package
 cd speex-1.2rc1/
 	./configure --prefix=$INSTALL_DDIR
