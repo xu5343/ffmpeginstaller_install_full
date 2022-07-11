1d362ceb8dbf3dcea8e6b5f7fb7d75db62c7fb7f
xu5343 <32597757+xu5343@users.noreply.github.com>
2022-07-11 22:16:18 +0800

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -55,6 +55,8 @@ cd ffmpeg-3.1
    cp -vf tools/qt-faststart /usr/local/cpffmpeg/bin/
    ln -sf /usr/local/cpffmpeg/bin/ffmpeg /usr/local/bin/ffmpeg
    ln -sf /usr/local/cpffmpeg/bin/ffmpeg /usr/bin/ffmpeg
+   ln -sf /usr/local/cpffmpeg/bin/ffprobe /usr/local/bin/ffprobe
+   ln -sf /usr/local/cpffmpeg/bin/ffprobe /usr/bin/ffprobe
    ln -sf /usr/local/cpffmpeg/bin/qt-faststart /usr/local/bin/qt-faststart
    ln -sf /usr/local/cpffmpeg/bin/qt-faststart /usr/bin/qt-faststart
    ldconfig
