3542a518b469f3432d6346af756c014ae8a5be8c
xu5343 <32597757+xu5343@users.noreply.github.com>
2022-07-11 23:01:58 +0800

    Update faad2.sh

--- faad2.sh
+++ faad2.sh
@@ -20,7 +20,7 @@ RED='\033[01;31m'
 RESET='\033[0m'
 INSTALL_SDIR='/usr/src/ffmpegscript'
 _url=`cat ./url.txt`
-SOURCE_URL=$_url/faad2
+SOURCE_URL=$_url
 INSTALL_DDIR='/usr/local/cpffmpeg'
 export cpu=`cat "/proc/cpuinfo" | grep "processor"|wc -l`
 export TMPDIR=$HOME/tmp
@@ -31,7 +31,6 @@ echo -e $RED"Installation of $_package ....... started"$RESET
 cd $INSTALL_SDIR/
 rm -rf faad2*
 wget $SOURCE_URL/$_package
-#mv /root/ffmpeginstaller/source/faad2/$_package $INSTALL_SDIR
 tar -xvzf $_package
 cd faad2-2.7/
 ./bootstrap
